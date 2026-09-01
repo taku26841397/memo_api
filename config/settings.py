935ca5e7046ff6b807c9197866d7d68bc72a4133
username <user@example>
2026-09-01 16:18:01 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0%fm+ysuv(-#fxv-@s81_!^-=60xzncihs9vzsi!$$1xt@1ln0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -129,3 +129,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+ALLOWED_HOSTS = ["memo-api-4okr.onrender.com"]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://memo-front.pages.dev",
+]
+
+
